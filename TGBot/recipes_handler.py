57bd30a0e9dc7b10f07d42242d2f2c258fe98ca7
AlekseevAlekz <botalelex@gmail.com>
2025-04-18 14:38:54 +0700

    First Commit

--- TGBot/recipes_handler.py
+++ TGBot/recipes_handler.py
@@ -1,61 +1,108 @@
-import aiohttp
+import asyncio
 import logging
+import random
+import aiohttp
+from aiogram.filters import Command, CommandObject
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from googletrans import Translator
 
-# Настройка логирования.
-logging.basicConfig(level=logging.INFO)
+from aiogram import Router
 
-URL = "https://www.themealdb.com/api/json/v1/1/"
+from TGBot.token_data import THEMEALDB_API
 
-async def fetch_json(url):
-    """Получает JSON данные по указанному URL."""
+router = Router()
+translator = Translator()
+
+
+class RecipeStates(StatesGroup):
+    waiting_for_category = State()
+    waiting_for_recipes = State()
+    showing_recipe_details = State()
+
+
+@router.message(Command('category_search_random'))
+async def category_search_random(message: Message, command: CommandObject, state: FSMContext):
+    if command.args is None:
+        await message.answer("Вы забыли указать количество рецептов!\n"
+                             "Пожалуйста повторите команду и передайте число.\n"
+                             "Пример: /category_search_random 2")
+
+        return
+
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                response.raise_for_status() # Поднимаем исключение для ошибок HTTP
-                return await response.json()
+        count_recipes = int(command.args)
+    except ValueError:
+        await message.answer(f"Не удалось распознать значение {command.args}\n"
+                             f"Пример: /category_search_random 2")
+        return
 
-    except aiohttp.ClientError as e:
-        logging.error(f"Ошибка при запросе к API: {e}")
-        return None
-    except Exception as e:
-        logging.error(f"Неожиданная ошибка: {e}")
-        return None
+    await state.set_data({'count_recipes': count_recipes})
 
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(f"{THEMEALDB_API}/categories.php") as response:
+                response.raise_for_status()
+                data = await response.json()
 
-async def get_categories():
-    """Получает список категорий блюд."""
-    url = f"{URL}list.php?c=list"
-    data = await fetch_json(url)
-    if data and data.get('meals'):
-        return data['meals']
-    return None
+                if 'categories' not in data or not data['categories']:
+                    await message.answer("Не удалось получить список категорий! Попробуйте еще раз.")
+                    return
 
+                categories = [category['strCategory'] for category in data['categories']]
 
-async def get_meals_by_category(category):
-    """Получает список блюд по категории."""
-    url = f"{URL}filter.php?c={category}"
-    data = await fetch_json(url)
-    if data and data.get('meals'):
-        return data['meals']
-    return None
+                builder = ReplyKeyboardBuilder()
+                for date_item in categories:
+                    builder.add(KeyboardButton(text=date_item))
+                builder.adjust(4)
 
-async def get_meal_details(meal_id):
-    """Получает детали блюда по ID"""
-    url = f"{URL}lookup.php?i={meal_id}"
-    data = await fetch_json(url)
-    if data and data.get('meals') and data['meals'][0]:
-        return data['meals'][0]
-    return None
+                await message.answer("Выберите категорию блюда:",
+                                     reply_markup=builder.as_markup(resize_keyboard=True))
+                await state.set_state(RecipeStates.waiting_for_category.state)
 
 
-async def get_random_meals_by_ids(meal_ids):
-    """Получает детали блюд по списку ID."""
-    import asyncio
-    results = await asyncio.gather(*[get_meal_details(meal_id) for meal_id in meal_ids])
-    return results
+        except aiohttp.ClientError as e:
+            logging.error(f"Ошибка при запросе к API: {e}")
+            await message.answer("Произошла ошибка при получении категорий. Попробуйте позже.")
+        except Exception as e:
+            logging.error(f"Неожиданная ошибка: {e}")
+            await message.answer("Произошла неожиданная ошибка. Попробуйте позже.")
 
 
-class CategorySearchStates(StatesGroup):
-    choosing_category = State()
-    showing_recipes = State()
+@router.message(RecipeStates.waiting_for_category)
+async def category_choosing_handler(message: Message, state: FSMContext):
+    category = message.text
+    count_recipes = (await state.get_data()).get('count_recipes', 1)
+
+    async with aiohttp.ClientSession as session:
+        try:
+            async with session.get(f"{THEMEALDB_API}/filter.php?c={category}") as response:
+                response.raise_for_status()
+                data = await response.json()
+
+                if 'meals' not in data or not data['meals']:
+                    await message.answer("Для этой категории не найдены рецепты!")
+                    return
+
+                meals = random.choices(data['meals'], k=count_recipes)
+                meal_names = [meal['strMeal'] for meal in meals]
+                meal_ids = [meal['idMeal'] for meal in meals]
+
+                await state.set_data({'meal_ids': meal_ids, 'meals': meals})
+
+                translated_names = [translator.translate(name, dest='ru').text for name in meal_names]
+
+                builder = ReplyKeyboardBuilder()
+                builder.add(KeyboardButton(text="Покажи рецепты."))
+                await message.answer(f"Как вам такие варианты: {','.join(translated_names)}",
+                                     reply_markup=builder.as_markup(resize_keyboard=True))
+
+
+        except aiohttp.ClientError as e:
+            logging.error(f"Ошибка при запросе к API: {e}")
+            await message.answer("Произошла ошибка при получении рецептов. Попробуйте позже.")
+        except Exception as e:
+            logging.error(f"Неожиданная ошибка: {e}")
+            await message.answer("Произошла неожиданная ошибка. Попробуйте позже.")
